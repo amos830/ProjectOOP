ba7b3c3d919b0ce00b026bbfbf4ec3e252eb8b45
Mak Chung Hang <chunghmak8-c@my.cityu.edu.hk>
2019-04-09 01:48:27 +0800

    Added Icon

--- QtGuiApplicationproject/GeneratedFiles/ui_ThankYouDialog.h
+++ QtGuiApplicationproject/GeneratedFiles/ui_ThankYouDialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'ThankYouDialog.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.2
+** Created by: Qt User Interface Compiler version 5.12.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -71,7 +71,7 @@ public:
     void retranslateUi(QDialog *ThankYouDialog)
     {
         ThankYouDialog->setWindowTitle(QApplication::translate("ThankYouDialog", "Dialog", nullptr));
-        ThankMessage->setText(QApplication::translate("ThankYouDialog", "TextLabel", nullptr));
+        ThankMessage->setText(QApplication::translate("ThankYouDialog", "Thank You For Using The System", nullptr));
         Exit->setText(QApplication::translate("ThankYouDialog", "Exit", nullptr));
     } // retranslateUi
 
