7d9bda2ef3ddb22225e3ab6f3859a138fcd80888
Mak Chung Hang <chunghmak8-c@my.cityu.edu.hk>
2019-04-13 04:18:29 +0800

    not tested

--- QtGuiApplicationproject/QtLoanRecordView.cpp
+++ QtGuiApplicationproject/QtLoanRecordView.cpp
@@ -38,21 +38,24 @@ void QtLoanRecordView::updateTable(bool admin) {
 	}
 	else {
 		int j;
-		//for (int i = 0; i < controller->records.size(); i++)
-			//if (controller->records.at(i));
-		table = new QStandardItemModel(controller->records.size(), 4, this);
+		for (int i = 0; i < controller->records.size(); i++)
+			if (controller->records.at(i).getName() == controller->CurrentUser->getName() && controller->records.at(i).getStatus() == "out");
+		j++;
+		table = new QStandardItemModel(j, 4, this);
 		table->setHorizontalHeaderItem(0, new QStandardItem(tr("Loan Date")));
 		table->setHorizontalHeaderItem(1, new QStandardItem(tr("ItemID")));
 		table->setHorizontalHeaderItem(2, new QStandardItem(tr("Item Name")));
 		table->setHorizontalHeaderItem(3, new QStandardItem(tr("Return Date")));
 		for (int i = 0; i < controller->records.size(), !controller->records.empty(); i++) {
-			table->setItem(i, 0, new QStandardItem(QString::fromStdString(controller->records.at(i).getLoanDate())));
-			table->setItem(i, 1, new QStandardItem(QString::fromStdString(controller->records.at(i).getId())));
-			table->setItem(i, 2, new QStandardItem(QString::fromStdString(controller->records.at(i).getName())));
-			table->setItem(i, 3, new QStandardItem(QString::fromStdString(controller->records.at(i).getNameOfBorrower())));
-			table->setItem(i, 4, new QStandardItem(QString::fromStdString(controller->records.at(i).getReturnDate())));
-			table->setItem(i, 5, new QStandardItem(QString::fromStdString(controller->records.at(i).getStatus())));
+			if (controller->records.at(i).getName() == controller->CurrentUser->getName() && controller->records.at(i).getStatus() == "out") {
+				table->setItem(j, 0, new QStandardItem(QString::fromStdString(controller->records.at(i).getLoanDate())));
+				table->setItem(j, 1, new QStandardItem(QString::fromStdString(controller->records.at(i).getId())));
+				table->setItem(j, 2, new QStandardItem(QString::fromStdString(controller->records.at(i).getName())));
+				table->setItem(j, 3, new QStandardItem(QString::fromStdString(controller->records.at(i).getReturnDate())));
+				j++;
+			}
 		}
+	}
 		LoanRecordView->setModel(table);
 		LoanRecordView->horizontalHeader()->setStretchLastSection(true);
 		LoanRecordView->resizeColumnsToContents();
@@ -60,5 +63,4 @@ void QtLoanRecordView::updateTable(bool admin) {
 		LoanRecordView->verticalHeader()->hide();
 		LoanRecordView->setSelectionMode(QAbstractItemView::SingleSelection);
 		LoanRecordView->setEditTriggers(QAbstractItemView::NoEditTriggers);
-	}
 }
